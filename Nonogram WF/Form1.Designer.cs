c5880352ef9ce7890987ab5f2146bf365a62fb42
Mischa Sasse <mmischaaaa4@gmail.com>
2025-03-07 16:08:29 +0100

    added login usercontrol

--- Nonogram WF/Form1.Designer.cs
+++ Nonogram WF/Form1.Designer.cs
@@ -20,20 +20,38 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			login1 = new login();
+			SuspendLayout();
+			// 
+			// login1
+			// 
+			login1.Dock = DockStyle.Fill;
+			login1.Location = new Point(0, 0);
+			login1.Name = "login1";
+			login1.Size = new Size(1378, 869);
+			login1.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(10F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1378, 869);
+			Controls.Add(login1);
+			Name = "Form1";
+			Text = "Form1";
+			ResumeLayout(false);
+		}
 
-        #endregion
-    }
+		#endregion
+
+		private login login1;
+	}
 }
--- Nonogram WF/Form1.cs
+++ Nonogram WF/Form1.cs
@@ -1,10 +1,16 @@
 namespace Nonogram_WF
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			//new login();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//panel1.Hide();
+		}
+	}
 }
