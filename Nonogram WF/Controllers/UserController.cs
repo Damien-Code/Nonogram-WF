0d086602ac41705bca5e49727a8f5620ba5d68df
Damien-Code <dovandenijssel@live.nl>
2025-03-12 16:00:19 +0100

    Added controllers and models for registration

---
+++ Nonogram WF/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nonogram_WF.Controllers
+{
+    public class UserController
+    {
+        // check if values are correct
+
+        // correct email/password
+    }
+}
---
+++ Nonogram WF/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nonogram_WF.Models
+{
+    internal class Users
+    {
+        // interface to check if everyting is correct
+        // set values to json
+    }
+}
