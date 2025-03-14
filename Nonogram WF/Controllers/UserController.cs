191ba6993b7a2cc5e43f7bb2c6368ee2ae68dca4
Damien-Code <dovandenijssel@live.nl>
2025-03-14 16:36:28 +0100

    Check if email exists

--- Nonogram WF/Controllers/UserController.cs
+++ Nonogram WF/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Nonogram_WF.Database;
 using Nonogram_WF.Models;
 namespace Nonogram_WF.Controllers
 {
@@ -24,26 +25,37 @@ namespace Nonogram_WF.Controllers
         /// <returns></returns>
         public static bool ValidateUserData(string email, string password, string confirmPassword)
         {
+            // If the email does not match the correct specifications, give error message
             if (!CheckEmail(email)) 
             {
                 MessageBox.Show("Please enter a valid email. \n It should be as following: johndoe@example.com ");
-                return false;//return email error
+                return false;
             }
 
+            // If the password does not match the correct specifications, give error message
             if (!CheckPassword(password))
             {
                 MessageBox.Show("Please enter a valid password. \n It should contain at least 6 characters");
-                return false;//return email error
+                return false;
             }
-            if(!CheckConfirmPassword(password, confirmPassword))
+
+            // If the the confirm password does not match the input of password, give error message
+            if (!CheckConfirmPassword(password, confirmPassword))
             {
                 MessageBox.Show("Please make sure that you confirm the correct password");
                 return false;
             }
 
-            Settings settings = new Settings("Dark", "16");
-            
+            // If the email already exists give error message
+            if (!CheckEmailExists(email)){
+                MessageBox.Show("This email already exists");
+                return false;
+            }
 
+            // check to see if settings work
+            Settings settings = new Settings("Dark", "16");
+
+            // If all the methods return true, then store the email and password(hash and salt)
             Users.SetUser(email, Users.CreatePassword(password), settings);
             return true;
         }
@@ -96,6 +108,23 @@ namespace Nonogram_WF.Controllers
         public static bool CheckConfirmPassword(string password, string confirmPassword)
         {
             return (password != confirmPassword) ? false : true;
+        }
+
+        /// <summary>
+        /// Goes through the users of the GetUsers method
+        /// Checks if the input email matches one of the already stored emails
+        /// Returns false if so
+        /// Else returns true
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public static bool CheckEmailExists(string email)
+        {
+            foreach(Users user in JSON_RW.GetUsers().Users)
+            {
+                if(user.Email != email) return false;
+            }
+            return true;
         }
 
 
