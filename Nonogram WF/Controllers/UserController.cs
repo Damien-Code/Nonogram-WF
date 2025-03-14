612d6abbbe5b58928fd960148187ce36ab7c6c1f
Damien-Code <dovandenijssel@live.nl>
2025-03-14 15:08:06 +0100

    created a check email and confirm email

--- Nonogram WF/Controllers/UserController.cs
+++ Nonogram WF/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Nonogram_WF.Models;
 namespace Nonogram_WF.Controllers
@@ -19,30 +21,61 @@ namespace Nonogram_WF.Controllers
         /// <param name="email"></param>
         /// <param name="password"></param>
         /// <returns></returns>
-        public static bool ValidateUserData(string email, string password)
+        public static bool ValidateUserData(string email, string password, string confirmPassword)
         {
-            if (!CheckEmail(email)) return false;//return email error
-            if (!CheckPassword(password)) return false ;//return email error
+            if (!CheckEmail(email)) 
+            {
+                MessageBox.Show("Please enter a valid email. \n It should be as following: johndoe@example.com ");
+                return false;//return email error
+            }
+
+            if (!CheckPassword(password))
+            {
+                MessageBox.Show("Please enter a valid password. \n It should contain at least 6 characters");
+                return false;//return email error
+            }
+            if(!CheckConfirmPassword(password, confirmPassword))
+            {
+                MessageBox.Show("Please make sure that you confirm the correct password");
+                return false;
+            }
+
+            
+
             Users.SetUser(email, Users.CreatePassword(password));
             return true;
         }
 
+
+        static Regex ValidEmailRegex = CreateValidEmailRegex();
+
+        private static Regex CreateValidEmailRegex()
+        {
+            string validEmailPattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            return new Regex(validEmailPattern, RegexOptions.IgnoreCase);
+        }
         // check if input values are correct
         public static bool CheckEmail(string email)
         {
-            if(email.Length < 6)
-            {
-                return false;
-            }
-            return true;
+            bool isValid = ValidEmailRegex.IsMatch(email);
+
+            return isValid;
         }
 
         // Check if input values are correct
         public static bool CheckPassword(string password)
         {
-            //if (password.Length < 6) { return false; }
-            //return true;
             return (password.Length < 6) ? false : true;
         }
+
+        public static bool CheckConfirmPassword(string password, string confirmPassword)
+        {
+            return (password != confirmPassword) ? false : true;
+        }
+
+
     }
 }
