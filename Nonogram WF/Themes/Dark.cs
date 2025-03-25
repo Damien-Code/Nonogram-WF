dc38d7c77465306b4bc45c3447d7897ad8d95f60
Mischa Sasse <mmischaaaa4@gmail.com>
2025-03-25 12:37:14 +0100

    added more themes

--- Nonogram WF/Themes/Dark.cs
+++ Nonogram WF/Themes/Dark.cs
@@ -9,21 +9,21 @@ namespace Nonogram_WF.Themes
 {
     class Dark : Theme
     {
-        //MyColor is color to flip
-        //Color.FromArgb(MyColor.ToArgb()^0xffffff)
+        //Int32 colorChangeAmount = 0xbababa; //grey
+        private Int32 colorChangeAmount = 0xffffff;
         public Dark() {
-            UserControlBG = Color.FromArgb(UserControl.DefaultBackColor.ToArgb() ^ 0xffffff);
-            UserControlFG = Color.FromArgb(UserControl.DefaultForeColor.ToArgb() ^ 0xffffff);
-            PanelBG = Color.FromArgb(Panel.DefaultBackColor.ToArgb() ^ 0xffffff);
-            PanelFG = Color.FromArgb(Panel.DefaultForeColor.ToArgb() ^ 0xffffff);
-            ButtonBG = Color.FromArgb(Button.DefaultBackColor.ToArgb() ^ 0xffffff);
-            ButtonFG = Color.FromArgb(Button.DefaultForeColor.ToArgb() ^ 0xffffff);
-            LabelBG = Color.FromArgb(Label.DefaultBackColor.ToArgb() ^ 0xffffff);
-            LabelFG = Color.FromArgb(Label.DefaultForeColor.ToArgb() ^ 0xffffff);
-            TextBoxBG = Color.FromArgb(TextBox.DefaultBackColor.ToArgb() ^ 0xffffff);
-            TextBoxFG = Color.FromArgb(TextBox.DefaultForeColor.ToArgb() ^ 0xffffff);
-            DataGridViewBG = Color.FromArgb(DataGridView.DefaultBackColor.ToArgb() ^ 0xffffff);
-            DataGridViewFG = Color.FromArgb(DataGridView.DefaultForeColor.ToArgb() ^ 0xffffff);
+            UserControlBG = Color.FromArgb(UserControl.DefaultBackColor.ToArgb() ^ colorChangeAmount);
+            UserControlFG = Color.FromArgb(UserControl.DefaultForeColor.ToArgb() ^ colorChangeAmount);
+            PanelBG = Color.FromArgb(Panel.DefaultBackColor.ToArgb() ^ colorChangeAmount);
+            PanelFG = Color.FromArgb(Panel.DefaultForeColor.ToArgb() ^ colorChangeAmount);
+            ButtonBG = Color.FromArgb(Button.DefaultBackColor.ToArgb() ^ colorChangeAmount);
+            ButtonFG = Color.FromArgb(Button.DefaultForeColor.ToArgb() ^ colorChangeAmount);
+            LabelBG = Color.FromArgb(Label.DefaultBackColor.ToArgb() ^ colorChangeAmount);
+            LabelFG = Color.FromArgb(Label.DefaultForeColor.ToArgb() ^ colorChangeAmount);
+            TextBoxBG = Color.FromArgb(TextBox.DefaultBackColor.ToArgb() ^ colorChangeAmount);
+            TextBoxFG = Color.FromArgb(TextBox.DefaultForeColor.ToArgb() ^ colorChangeAmount);
+            DataGridViewBG = Color.FromArgb(DataGridView.DefaultBackColor.ToArgb() ^ colorChangeAmount);
+            DataGridViewFG = Color.FromArgb(DataGridView.DefaultForeColor.ToArgb() ^ colorChangeAmount);
         }
     }
 }
