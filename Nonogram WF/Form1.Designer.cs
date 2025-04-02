11fc6fe6be753ce7bd9b2abfe43711e9e79f4902
Damien-Code <dovandenijssel@live.nl>
2025-04-02 15:40:39 +0200

    adjusted the form load to the users screen size and removed resize

--- Nonogram WF/Form1.Designer.cs
+++ Nonogram WF/Form1.Designer.cs
@@ -38,87 +38,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Login = new Views.Login();
-            Welcome = new Views.Welcome();
-            Instructions = new Views.Instructions();
-            Home = new Views.Home();
-            Difficulty = new Views.Difficulty();
-            GameScreen = new Views.GameScreen();
-            Settings = new Views.Settings();
             Registration = new Views.Registration();
-            history1 = new Views.History();
             SuspendLayout();
-            // 
-            // Login
-            // 
-            Login.Dock = DockStyle.Fill;
-            Login.Location = new Point(0, 0);
-            Login.Margin = new Padding(0);
-            Login.Name = "Login";
-            Login.Size = new Size(784, 436);
-            Login.TabIndex = 0;
-            Login.Visible = false;
-            // 
-            // Welcome
-            // 
-            Welcome.BackColor = Color.White;
-            Welcome.Dock = DockStyle.Fill;
-            Welcome.Location = new Point(0, 0);
-            Welcome.Margin = new Padding(1, 0, 1, 0);
-            Welcome.Name = "Welcome";
-            Welcome.Size = new Size(784, 436);
-            Welcome.TabIndex = 0;
-            Welcome.Load += welcome2_Load;
-            // 
-            // Instructions
-            // 
-            Instructions.Dock = DockStyle.Fill;
-            Instructions.Location = new Point(0, 0);
-            Instructions.Margin = new Padding(1, 0, 1, 0);
-            Instructions.Name = "Instructions";
-            Instructions.Size = new Size(784, 436);
-            Instructions.TabIndex = 1;
-            Instructions.Visible = false;
-            // 
-            // Home
-            // 
-            Home.Dock = DockStyle.Fill;
-            Home.Location = new Point(0, 0);
-            Home.Margin = new Padding(1, 0, 1, 0);
-            Home.Name = "Home";
-            Home.Size = new Size(784, 436);
-            Home.TabIndex = 2;
-            Home.Visible = false;
-            // 
-            // Difficulty
-            // 
-            Difficulty.Dock = DockStyle.Fill;
-            Difficulty.Location = new Point(0, 0);
-            Difficulty.Margin = new Padding(1, 0, 1, 0);
-            Difficulty.Name = "Difficulty";
-            Difficulty.Size = new Size(784, 436);
-            Difficulty.TabIndex = 3;
-            Difficulty.Visible = false;
-            // 
-            // GameScreen
-            // 
-            GameScreen.Dock = DockStyle.Fill;
-            GameScreen.Location = new Point(0, 0);
-            GameScreen.Margin = new Padding(1, 0, 1, 0);
-            GameScreen.Name = "GameScreen";
-            GameScreen.Size = new Size(784, 436);
-            GameScreen.TabIndex = 4;
-            GameScreen.Visible = false;
-            // 
-            // Settings
-            // 
-            Settings.Dock = DockStyle.Fill;
-            Settings.Location = new Point(0, 0);
-            Settings.Margin = new Padding(1, 0, 1, 0);
-            Settings.Name = "Settings";
-            Settings.Size = new Size(784, 436);
-            Settings.TabIndex = 6;
-            Settings.Visible = false;
             // 
             // Registration
             // 
@@ -130,33 +51,16 @@
             Registration.TabIndex = 7;
             Registration.Visible = false;
             // 
-            // history1
-            // 
-            history1.Dock = DockStyle.Fill;
-            history1.Location = new Point(0, 0);
-            history1.Margin = new Padding(2, 1, 2, 1);
-            history1.Name = "history1";
-            history1.Size = new Size(784, 436);
-            history1.TabIndex = 8;
-            history1.Visible = false;
-            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(784, 436);
-            Controls.Add(Welcome);
-            Controls.Add(Home);
-            Controls.Add(Instructions);
-            Controls.Add(Login);
-            Controls.Add(GameScreen);
-            Controls.Add(Settings);
             Controls.Add(Registration);
-            Controls.Add(Difficulty);
-            Controls.Add(history1);
             Margin = new Padding(2, 1, 2, 1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
--- Nonogram WF/Form1.cs
+++ Nonogram WF/Form1.cs
@@ -7,7 +7,11 @@ namespace Nonogram_WF
     {
         public Form1()
         {
+            //this.MinimumSize = new Size(900, 450);
+            
+            
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -18,6 +22,16 @@ namespace Nonogram_WF
         private void welcome2_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = Screen.PrimaryScreen.WorkingArea.Width / 2;
+            this.Height = Screen.PrimaryScreen.WorkingArea.Height / 2;
+            this.Top = (Screen.PrimaryScreen.WorkingArea.Top + Screen.PrimaryScreen.WorkingArea.Height) / 4;
+            this.Left = (Screen.PrimaryScreen.WorkingArea.Left + Screen.PrimaryScreen.WorkingArea.Width) / 4;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
         }
     }
 }
