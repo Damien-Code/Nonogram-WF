952c292fb804c7ac5a1e0b322affaa7c9899a6aa
Mischa Sasse <mmischa1@gmail.com>
2025-04-04 15:31:08 +0200

    fixed damiens acceptatietest

--- Nonogram_Tests/Acceptatietests/AcceptatieTest.cs
+++ Nonogram_Tests/Acceptatietests/AcceptatieTest.cs
@@ -3,8 +3,10 @@ using Nonogram_WF.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Nonogram_Tests
 {
@@ -24,6 +26,16 @@ namespace Nonogram_Tests
             Users? y = Session.GetSession();
 
             Assert.IsTrue(y.Email == "" && y.Password == null);
+        }
+        [TestMethod]
+        public void Test_HomeToDifficulty()
+        {
+            Nonogram_WF.Views.Difficulty Diff = new Nonogram_WF.Views.Difficulty();
+            Nonogram_WF.Views.Home       home = new Nonogram_WF.Views.Home();
+            string destination = Diff.GetType().Name;
+            string userControl = home.AcceptatieTestButtonTest();
+            //assert
+            Assert.IsTrue(userControl == destination);
         }
     }
 }
